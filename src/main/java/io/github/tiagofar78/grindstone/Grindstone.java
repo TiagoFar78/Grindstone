e08ddbbca558a831dbb3ef941475d1192e1ef338
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2026-02-17 23:30:57 +0000

    fix fixed dependencies and command register

--- src/main/java/io/github/tiagofar78/grindstone/Grindstone.java
+++ src/main/java/io/github/tiagofar78/grindstone/Grindstone.java
@@ -5,13 +5,19 @@ import io.github.tiagofar78.grindstone.commands.LeaveQueueCommand;
 import io.github.tiagofar78.grindstone.listener.PlayerConnectionListener;
 import io.github.tiagofar78.grindstone.party.PartyService;
 import io.github.tiagofar78.messagesrepo.MessagesRepo;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Grindstone extends JavaPlugin {
 
@@ -19,6 +25,8 @@ public class Grindstone extends JavaPlugin {
 
     private static final String LEAVE_QUEUE_COMMAND_LABEL = "dequeue";
     private static final String FORCE_STOP_COMMAND_LABEL = "forcestop";
+
+    private static final Map<JavaPlugin, Map<String, CommandExecutor>> pluginCommands = new HashMap<>();
 
     private static Grindstone instance;
 
@@ -36,8 +44,8 @@ public class Grindstone extends JavaPlugin {
 
         instance = (Grindstone) Bukkit.getServer().getPluginManager().getPlugin("TF_Grindstone");
 
-        registerCommand(LEAVE_QUEUE_COMMAND_LABEL, new LeaveQueueCommand());
-        registerCommand(FORCE_STOP_COMMAND_LABEL, new ForceStopCommand());
+        registerCommand(this, LEAVE_QUEUE_COMMAND_LABEL, new LeaveQueueCommand());
+        registerCommand(this, FORCE_STOP_COMMAND_LABEL, new ForceStopCommand());
 
         GrindstoneConfig.load();
 
@@ -49,8 +57,40 @@ public class Grindstone extends JavaPlugin {
         instance.getServer().getPluginManager().registerEvents(listener, instance);
     }
 
-    public static void registerCommand(String label, CommandExecutor executor) {
-        instance.getCommand(label).setExecutor(executor);
+    public static void registerCommands(JavaPlugin plugin) {
+        plugin.getLifecycleManager().registerEventHandler(
+                LifecycleEvents.COMMANDS,
+                event -> {
+
+                    for (Map.Entry<String, CommandExecutor> entry : pluginCommands.get(plugin).entrySet()) {
+
+                        String label = entry.getKey();
+                        CommandExecutor executor = entry.getValue();
+
+                        event.registrar().register(
+                                Commands.literal(label).executes(ctx -> {
+
+                                    CommandSender sender = ctx.getSource().getSender();
+
+                                    String input = ctx.getInput();
+                                    String[] split = input.split(" ");
+                                    String[] args = Arrays.copyOfRange(split, 1, split.length);
+
+                                    executor.onCommand(sender, null, label, args);
+                                    return 1;
+                                }).build()
+                        );
+                    }
+                }
+        );
+    }
+
+    public static void registerCommand(JavaPlugin plugin, String label, CommandExecutor executor) {
+        if (!pluginCommands.containsKey(plugin)) {
+            pluginCommands.put(plugin, new HashMap<>());
+        }
+
+        pluginCommands.get(plugin).put(label, executor);
     }
 
 }
--- src/main/java/io/github/tiagofar78/grindstone/commands/MinigameCommandBinder.java
+++ src/main/java/io/github/tiagofar78/grindstone/commands/MinigameCommandBinder.java
@@ -7,59 +7,73 @@ import io.github.tiagofar78.grindstone.game.MinigameSettings;
 import io.github.tiagofar78.grindstone.queue.LobbyBasedQueue;
 import io.github.tiagofar78.grindstone.queue.MatchmakingQueue;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.File;
 import java.util.List;
 
 public class MinigameCommandBinder {
 
     public static void bindQueueCommands(
+            JavaPlugin plugin,
             MinigameFactory factory,
             MinigameSettings gamemode,
             List<MapFactory> availableMaps,
             String commandLabel
     ) {
         MatchmakingQueue queue = new LobbyBasedQueue(factory, gamemode, availableMaps);
-        bindQueueCommands(availableMaps, commandLabel, queue);
+        bindQueueCommands(plugin, availableMaps, commandLabel, queue);
     }
 
-    public static void bindQueueCommands(List<MapFactory> availableMaps, String commandLabel, MatchmakingQueue queue) {
+    public static void bindQueueCommands(
+            JavaPlugin plugin,
+            List<MapFactory> availableMaps,
+            String commandLabel,
+            MatchmakingQueue queue
+    ) {
         if (commandLabel.contains(" ") || commandLabel.isEmpty()) {
             throw new IllegalArgumentException(
                     "Could not register gamemode with command label \"" + commandLabel + "\". A command cannot contain spaces."
             );
         }
 
-        Grindstone.registerCommand(commandLabel, new JoinQueueCommand(queue));
+        Grindstone.registerCommand(plugin, commandLabel, new JoinQueueCommand(queue));
 
         for (MapFactory map : availableMaps) {
             String label = commandLabel + "_" + map.getName().toLowerCase();
-            Grindstone.registerCommand(label, new JoinQueueCommand(queue, map));
+            Grindstone.registerCommand(plugin, label, new JoinQueueCommand(queue, map));
         }
     }
 
     public static void bindForceStartCommands(
+            JavaPlugin plugin,
             MinigameFactory factory,
             MinigameSettings gamemode,
             List<MapFactory> availableMaps,
             String commandLabel
     ) {
         commandLabel = ForceStartCommand.COMMAND_LABEL + "_" + commandLabel;
-        Grindstone.registerCommand(commandLabel, new ForceStartCommand(factory, gamemode, availableMaps));
+        Grindstone.registerCommand(plugin, commandLabel, new ForceStartCommand(factory, gamemode, availableMaps));
 
         for (MapFactory map : availableMaps) {
             String label = commandLabel + "_" + map.getName().toLowerCase();
-            Grindstone.registerCommand(label, new ForceStartCommand(factory, gamemode, map));
+            Grindstone.registerCommand(plugin, label, new ForceStartCommand(factory, gamemode, map));
         }
     }
 
     public static void bindPasteMapsCommand(
+            JavaPlugin plugin,
             File pluginFolder,
             String gamemodeName,
             List<MapFactory> maps,
             String worldName
     ) {
         String commandLabel = PasteMapsCommand.COMMAND_LABEL + "_" + gamemodeName;
-        Grindstone.registerCommand(commandLabel, new PasteMapsCommand(pluginFolder, gamemodeName, maps, worldName));
+        Grindstone.registerCommand(
+                plugin,
+                commandLabel,
+                new PasteMapsCommand(pluginFolder, gamemodeName, maps, worldName)
+        );
     }
 
 }
